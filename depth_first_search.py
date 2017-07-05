4e8040e6171b6c9f7a9a8d9268e4f0f254671f59
Sam Zuckerman <sam.p.zuckerman@gmail.com>
2017-07-05 16:46:54 -0400

    working traverse_tree function added

--- depth_first_search.py
+++ depth_first_search.py
@@ -1,7 +1,8 @@
 class Node:
-	def __init__(self, name, nodes=[]):
+	def __init__(self, name, nodes=[], number=None):
 		self.name = name
 		self.nodes = nodes
+		self.number = number
 	def __repr__(self):
 		return "<Node %s>" % self.name
 
@@ -34,3 +35,39 @@ I.nodes = []
 
 node_set = {A, B, C, D, E, F, G, H, I}
 my_tree = Tree(nodes=node_set, HEAD = A)
+
+
+STACK = []
+count = 1
+
+starting_node = my_tree.HEAD
+starting_node.number = count
+count+=1
+
+
+def traverse_tree(starting_node):
+	global count
+	for node in starting_node.nodes:
+		if node.number is not None:
+			continue
+		# We're stacking from right to left
+		STACK.append(node)
+		print("Added %s" % node)
+	print("Current Stack: [%s]" % ', '.join([i.name for i in STACK]))
+	if STACK:
+		new_node=STACK.pop()
+		if new_node.number is None:
+			new_node.number=count
+			count+=1
+		return traverse_tree(new_node)
+	else:
+		print("Stack empty.  Ending recursion.")
+		return
+
+traverse_tree(starting_node)
+
+number_dict = {node.name: node.number for node in my_tree.nodes}
+
+for k, v in sorted(number_dict.items()):
+	print("Node %s: %s" % (k, v))
+
